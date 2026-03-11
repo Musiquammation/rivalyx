b2f612250ad9476116d74bbc36431d95d5f9547f
Musiquammation <bryanmegherbi@gmail.com>
2026-03-11 12:45:53 +0100

    Minor change

--- src/client/clientGameList.ts
+++ src/client/clientGameList.ts
@@ -1,6 +1,8 @@
+import { packice_client } from "../games/gpackice/packice_client";
 import { test_client } from "../games/gtest/test_client";
 import { ClientInterface } from "./ClientInterface";
 
 export const CLIENT_DESCRIPTIONS: ClientInterface<any, any>[] = [
-    test_client
+	packice_client,
+	test_client
 ];
